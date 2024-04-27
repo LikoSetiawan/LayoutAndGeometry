ab236523cb0d709a87c332817d8d5f3f9a1abc63
LikoSetiawan <128354782+LikoSetiawan@users.noreply.github.com>
2024-04-27 17:00:13 +0800

    learn geometry reader

--- ContentView.swift
+++ ContentView.swift
@@ -7,15 +7,73 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+extension VerticalAlignment {
+    struct MidAccountAndName: AlignmentID {
+        static func defaultValue(in context: ViewDimensions) -> CGFloat {
+            context[.top]
         }
-        .padding()
+    }
+    
+    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
+}
+
+struct OuterView: View {
+    var body: some View {
+        VStack{
+            Text("top")
+            
+            InnerView()
+                .background(.green)
+            
+            Text("bottom")
+        }
+    }
+}
+
+struct InnerView: View {
+    var body: some View {
+        HStack{
+            Text("left")
+            
+            GeometryReader { proxy in
+                Text("Center")
+                    .background(.blue)
+                    .onTapGesture {
+                        print("Global center: \(proxy.frame(in: .global).midX) x \(proxy.frame(in: .global).midY)")
+                        print("Custom center: \(proxy.frame(in: .named("Custom")).midX) x \(proxy.frame(in: .named("Custom")).midY)")
+                        print("Local center: \(proxy.frame(in: .local).midX) x \(proxy.frame(in: .local).midY)")
+                    }
+            }
+            .background(.orange)
+            
+            Text("Right")
+        }
+    }
+}
+
+
+struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .purple, .pink, .orange, .yellow]
+    
+    
+    var body: some View {
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
+                    GeometryReader { proxy in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(colors[index % 7])
+                            .rotation3DEffect(.degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            .opacity(proxy.frame(in: .global).minY / 200)
+                            .scaleEffect(max(0.5, proxy.frame(in: .global).minY / 400))
+                            .background(Color(hue: min(1, proxy.frame(in: .global).minY / fullView.size.height), saturation: 1, brightness: 1))
+                    }
+                    .frame(height: 40)
+                }
+            }
+        }
     }
 }
 
